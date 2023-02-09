8fb4ec7cf733785731ed28c142f6357fd51e4172
nelsonfrz <117679845+nelsonfrz@users.noreply.github.com>
2023-02-09 17:18:59 +0100

    Fixed index page bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,67 +30,73 @@ const Home: NextPage = () => {
       </Head>
       <Mantine.Center>
         <Mantine.Stack w={500}>
-          <Mantine.Title>Welcome {sessionData?.user.name}!</Mantine.Title>
-
-          <Mantine.Title order={2}>Create Paste</Mantine.Title>
-          <Mantine.TextInput 
-            ref={titleRef}
-            label='Title'
-            placeholder='Title'
-            onChange={e => {
-              setTitle(e.currentTarget.value);
-              setTitleError("");
-            }}
-            error={titleError}
-            />
-          <Mantine.Textarea
-            ref={contentRef}
-            label='Content'
-            placeholder='Content'
-            minRows={5}
-            autosize
-            onChange={e => {
-              setContent(e.currentTarget.value);
-              setContentError("");
-            }}
-            error={contentError}
-            />
-
-          <Mantine.Flex 
-            gap="xl"
-            justify="center"
-            align="center"
-            wrap='wrap'
-          >
-            <Mantine.Button 
-              color='green'
-              onClick={() => {
-                if (title.trim().length < 1) {
-                  setTitleError('Please enter title for paste.');
-                }
-                if (title.trim().length > 100) {
-                  setTitleError('Title is too long. 100 characters max.');
-                }
-                if (content.length < 1) {
-                  setContentError('Please enter content for paste.');
-                }
-                if (title.trim().length >= 1 && title.trim().length <= 100 && content.length >= 1) {
-                  void createMutation.mutateAsync({
-                    title,
-                    content
-                  }).then(paste => {
-                    void router.push(`/paste/${paste.id}`);
-                  });
-                }
+          {sessionData?.user.name ?
+          <>
+            <Mantine.Title>Welcome {sessionData?.user.name}!</Mantine.Title>
+            <Mantine.Title order={2}>Create Paste</Mantine.Title>
+            <Mantine.TextInput 
+              ref={titleRef}
+              label='Title'
+              placeholder='Title'
+              onChange={e => {
+                setTitle(e.currentTarget.value);
+                setTitleError("");
               }}
-            >Create Paste</Mantine.Button>
+              error={titleError}
+              />
+            <Mantine.Textarea
+              ref={contentRef}
+              label='Content'
+              placeholder='Content'
+              minRows={5}
+              autosize
+              onChange={e => {
+                setContent(e.currentTarget.value);
+                setContentError("");
+              }}
+              error={contentError}
+              />
 
-            <Link href={`/user/${sessionData?.user.id ?? ''}`}>
-              <Mantine.Button>See Your Pastes</Mantine.Button>
-            </Link>  
-              
-            <SignOutButton />
-          </Mantine.Flex>
+            <Mantine.Flex 
+              gap="xl"
+              justify="center"
+              align="center"
+              wrap='wrap'
+            >
+              <Mantine.Button 
+                color='green'
+                onClick={() => {
+                  if (title.trim().length < 1) {
+                    setTitleError('Please enter title for paste.');
+                  }
+                  if (title.trim().length > 100) {
+                    setTitleError('Title is too long. 100 characters max.');
+                  }
+                  if (content.length < 1) {
+                    setContentError('Please enter content for paste.');
+                  }
+                  if (title.trim().length >= 1 && title.trim().length <= 100 && content.length >= 1) {
+                    void createMutation.mutateAsync({
+                      title,
+                      content
+                    }).then(paste => {
+                      void router.push(`/paste/${paste.id}`);
+                    });
+                  }
+                }}
+              >Create Paste</Mantine.Button>
+
+              <Link href={`/user/${sessionData?.user.id ?? ''}`}>
+                <Mantine.Button>See Your Pastes</Mantine.Button>
+              </Link>  
+                
+              <SignOutButton />
+            </Mantine.Flex>
+          </>
+          :
+          <Mantine.Title>Loading...</Mantine.Title>
+          }
+          
         </Mantine.Stack>
       </Mantine.Center>
     </>
